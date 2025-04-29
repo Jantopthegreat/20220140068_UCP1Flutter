7c6faac9ce08276505ea988519e9ff649b15e3ee
Jantopthegreat <fauzangriya@gmail.com>
2025-04-29 22:08:16 +0700

    menyusun tampilan dan tata letak detailbrg

--- lib/presentation/barang/detailbrg.dart
+++ lib/presentation/barang/detailbrg.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:ucp1/presentation/homepage.dart';
 
 class DetailBrg extends StatelessWidget {
   final DateTime tanggal;
@@ -24,6 +26,62 @@ class DetailBrg extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final dateFormatted = DateFormat('EEEE, dd MMMM yyyy', 'id_ID').format(tanggal);
+    return Scaffold(
+      backgroundColor: Color(0xFFFEF8F6),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 32),
+          child: Column(
+            children: [
+              Spacer(),
+              Icon(Icons.check_circle, color: Colors.green, size: 100),
+              SizedBox(height: 16),
+              Text("Data Berhasil Disimpan", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              SizedBox(height: 24),
+              buildRow("Tanggal", dateFormatted),
+              buildRow("Jenis Transaksi", jenisTransaksi),
+              buildRow("Jenis Barang", jenisBarang),
+              buildRow("Jumlah Barang", jumlah.toString()),
+              buildRow("Harga Satuan", 'Rp. $hargaSatuan'),
+              buildRow("Total Harga", 'Rp. $totalHarga'),
+              Spacer(),
+              ElevatedButton(
+                onPressed: () => 
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomePage(
+                        username: username,
+                      ),
+                    ),
+                    (Route<dynamic> route) => false,
+                ),
+                
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  minimumSize: Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                ),
+                child: Text("Selesai"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildRow(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(label, style: TextStyle(fontWeight: FontWeight.w600)),
+          Text(value),
+        ],
+      ),
+    );
   }
 }
