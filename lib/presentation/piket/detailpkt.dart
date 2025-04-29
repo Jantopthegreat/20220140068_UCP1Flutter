1f1209cbdc7fdbdae49a3acd4edb59d33a46be22
Jantopthegreat <fauzangriya@gmail.com>
2025-04-29 21:49:17 +0700

    membuat variabel email, tanggal, dan tugas

--- lib/presentation/piket/detailpkt.dart
+++ lib/presentation/piket/detailpkt.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
 class DetailPkt extends StatelessWidget {
-  const DetailPkt({super.key});
+  final String email;
+  final String tanggal;
+  final String tugas;
+
+  const DetailPkt({
+    Key? key,
+    required this.email,
+    required this.tanggal,
+    required this.tugas,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
