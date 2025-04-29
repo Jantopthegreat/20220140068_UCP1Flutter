8e4b34e2274775b0de497e98bd868e3568580f84
Jantopthegreat <fauzangriya@gmail.com>
2025-04-29 21:52:26 +0700

    membuat homeplg page statefulwidget

---
+++ lib/presentation/pelanggan/homeplg.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePlg extends StatefulWidget {
+  const HomePlg({super.key});
+
+  @override
+  State<HomePlg> createState() => _HomePlgState();
+}
+
+class _HomePlgState extends State<HomePlg> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
