8ccbe3b368a281b551995a1ff9770cf8f0017bb7
Jantopthegreat <fauzangriya@gmail.com>
2025-04-29 21:40:40 +0700

    membuat fungsi, text editing controller, dll untuk home pkt

--- lib/presentation/piket/homepkt.dart
+++ lib/presentation/piket/homepkt.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
 class HomePkt extends StatefulWidget {
-  const HomePkt({super.key});
+  final String email;
+  const HomePkt({Key? key, required this.email}) : super(key: key);
 
   @override
   State<HomePkt> createState() => _HomePktState();
 }
 
 class _HomePktState extends State<HomePkt> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _namaController = TextEditingController();
+  final TextEditingController _tugasController = TextEditingController();
+  DateTime? _selectedDate;
+  List<String> _daftarTugas = [];
+
+  bool _tanggalError = false;
+  bool _tugasError = false;
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
