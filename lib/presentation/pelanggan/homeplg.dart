63a30fcf40307484fc31cb3999b38a62c4609e3c
Jantopthegreat <fauzangriya@gmail.com>
2025-04-29 21:53:56 +0700

    membuat variabel dan text editing controller

--- lib/presentation/pelanggan/homeplg.dart
+++ lib/presentation/pelanggan/homeplg.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 
 class HomePlg extends StatefulWidget {
-  const HomePlg({super.key});
+  final String username;
+  final String avatarPath;
+
+  const HomePlg({
+    super.key,
+    required this.username,
+    required this.avatarPath,
+  });
 
   @override
   State<HomePlg> createState() => _HomePlgState();
 }
 
 class _HomePlgState extends State<HomePlg> {
+  final _formKey = GlobalKey<FormState>();
+  final _namaController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _hpController = TextEditingController();
+  final _alamatController = TextEditingController();
+  final _provinsiController = TextEditingController();
+  final _kodePosController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
