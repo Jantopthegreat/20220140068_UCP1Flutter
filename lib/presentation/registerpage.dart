982f7c159d86856d2b8b6de13fcb17b0d9fc6839
Jantopthegreat <fauzangriya@gmail.com>
2025-04-29 20:56:07 +0700

    membuat body dan memasukan foto

--- lib/presentation/registerpage.dart
+++ lib/presentation/registerpage.dart
@@ -18,3 +18,23 @@ class _RegisterPageState extends State<RegisterPage> {
 
   bool _obscurePassword = true;
   bool _obscureConfirmPassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFFEF8F7),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const SizedBox(height: 60),
+              Image(
+                image: AssetImage('assets/images/gunung.jpg'),
+                width: 200,
+                height: 200,
+              ),
