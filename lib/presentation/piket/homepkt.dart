4df3273c17fd0ed09bfa4d8abafb9af5d6999598
Jantopthegreat <fauzangriya@gmail.com>
2025-04-29 21:38:04 +0700

    membuat HomePiket Page statefulwidget

---
+++ lib/presentation/piket/homepkt.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePkt extends StatefulWidget {
+  const HomePkt({super.key});
+
+  @override
+  State<HomePkt> createState() => _HomePktState();
+}
+
+class _HomePktState extends State<HomePkt> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
