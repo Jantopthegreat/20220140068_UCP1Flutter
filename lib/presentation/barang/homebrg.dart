02d204138dbbc68ed99ad701097cd8790e96e4ce
Jantopthegreat <fauzangriya@gmail.com>
2025-04-29 22:01:00 +0700

    membuat homebrg statefulwidget

---
+++ lib/presentation/barang/homebrg.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeBrg extends StatefulWidget {
+  const HomeBrg({super.key});
+
+  @override
+  State<HomeBrg> createState() => _HomeBrgState();
+}
+
+class _HomeBrgState extends State<HomeBrg> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
