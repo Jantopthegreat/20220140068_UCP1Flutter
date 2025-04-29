d5937768d2bbb98d2ad690065ad8d24fa6b3b2cc
Jantopthegreat <fauzangriya@gmail.com>
2025-04-29 21:58:28 +0700

    membuat variabel username, dll

--- lib/presentation/pelanggan/detailplg.dart
+++ lib/presentation/pelanggan/detailplg.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 
 class DetailPlg extends StatelessWidget {
-  const DetailPlg({super.key});
+  final String username;
+  final String avatarPath;
+  final String nama;
+  final String email;
+  final String hp;
+  final String alamat;
+  final String provinsi;
+  final String kodePos;
+
+  const DetailPlg({
+    super.key,
+    required this.username,
+    required this.avatarPath,
+    required this.nama,
+    required this.email,
+    required this.hp,
+    required this.alamat,
+    required this.provinsi,
+    required this.kodePos,
+  });
+
 
   @override
   Widget build(BuildContext context) {
