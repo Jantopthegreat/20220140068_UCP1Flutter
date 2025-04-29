f8ec5ab4f1470d7d453c6c916f6bc38c115e74a6
Jantopthegreat <fauzangriya@gmail.com>
2025-04-29 21:45:49 +0700

    menyusun tampilan dan tata letak homepkt page

--- lib/presentation/piket/homepkt.dart
+++ lib/presentation/piket/homepkt.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:ucp1/presentation/piket/detailpkt.dart';
 
 class HomePkt extends StatefulWidget {
   final String email;
@@ -44,6 +45,180 @@ class _HomePktState extends State<HomePkt> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        toolbarHeight: 90,
+        title: const Text('Piket Gudang', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              const Text(
+                'Nama Anggota',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              TextField(
+                controller: _namaController,
+                readOnly: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Pilih Tanggal',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              GestureDetector(
+                onTap: () {
+                  _pickDate();
+                  setState(() {
+                    _tanggalError = false;
+                  });
+                },
+                child: InputDecorator(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    suffixIcon: const Icon(Icons.calendar_today),
+                    errorText:
+                        _tanggalError ? 'Tanggal tidak boleh kosong' : null,
+                  ),
+                  child: Text(
+                    _selectedDate == null
+                        ? 'Pilih Tanggal'
+                        : DateFormat('EEEE, dd MMMM yyyy', 'id_ID')
+                            .format(_selectedDate!),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Tugas Piket',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _tugasController,
+                      decoration: InputDecoration(
+                        hintText: 'Tugas Piket',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        errorText: _tugasError
+                            ? 'Tugas piket tidak boleh kosong'
+                            : null,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepOrange,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 18,
+                        horizontal: 70,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _tugasError = _tugasController.text.isEmpty;
+                        _tanggalError = _selectedDate == null;
+                      });
+
+                      if (!_tugasError && !_tanggalError) {
+                        setState(() {
+                          _daftarTugas.add(_tugasController.text);
+                          _tugasController.clear();
+                          _tugasError = false;
+                        });
+                      }
+                    },
+                    child: const Text(
+                      'Tambah',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30),
+              const Center(
+                child: Text(
+                  'Daftar Tugas Piket',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+              ),
+              const SizedBox(height: 12),
+              _daftarTugas.isEmpty
+                  ? SizedBox(
+                      height: 400,
+                      child: Center(
+                        child: Text(
+                          'Belum ada data',
+                          style: TextStyle(fontSize: 13),
+                        ),
+                      ),
+                    )
+                  : Column(
+                      children: _daftarTugas
+                          .map(
+                            (tugas) => Card(
+                              color: Colors.deepOrange,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              child: ListTile(
+                                title: Text(
+                                  tugas,
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                                trailing: const Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: Colors.white,
+                                  size: 15,
+                                ),
+                                onTap: () {
+                                  if (_selectedDate != null) {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => DetailPkt(
+                                          email: _namaController.text,
+                                          tanggal: DateFormat(
+                                            'EEEE, dd MMMM yyyy',
+                                            'id_ID',
+                                          ).format(_selectedDate!),
+                                          tugas: tugas,
+                                        ),
+                                      ),
+                                    );
+                                  }
+                                },
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
