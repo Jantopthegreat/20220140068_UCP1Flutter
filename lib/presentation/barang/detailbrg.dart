cc31b12fe95afefba8fa49480f068907a887a29f
Jantopthegreat <fauzangriya@gmail.com>
2025-04-29 22:07:11 +0700

    membuat variabel tanggall, dll

--- lib/presentation/barang/detailbrg.dart
+++ lib/presentation/barang/detailbrg.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
 class DetailBrg extends StatelessWidget {
-  const DetailBrg({super.key});
+  final DateTime tanggal;
+  final String jenisTransaksi;
+  final String jenisBarang;
+  final int jumlah;
+  final int hargaSatuan;
+  final int totalHarga;
+  final String username;
+  final String avatarPath;
+
+  const DetailBrg({
+    Key? key,
+    required this.tanggal,
+    required this.username,
+    required this.avatarPath,
+    required this.jenisTransaksi,
+    required this.jenisBarang,
+    required this.jumlah,
+    required this.hargaSatuan,
+    required this.totalHarga,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
