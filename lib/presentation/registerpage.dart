d369855521bc65386cd3a77cda67715a8d249fd9
Jantopthegreat <fauzangriya@gmail.com>
2025-04-29 20:52:33 +0700

    membuat RegisterPage stetafulwidget

---
+++ lib/presentation/registerpage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:ucp1/presentation/loginpage.dart';
+import 'package:ucp1/presentation/homepage.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
