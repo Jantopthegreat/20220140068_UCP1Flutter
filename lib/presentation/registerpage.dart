72e47774abb053c80a948ef5da4366c7e891a0ca
Jantopthegreat <fauzangriya@gmail.com>
2025-04-29 20:53:34 +0700

    membuat text editing controller untuk register

--- lib/presentation/registerpage.dart
+++ lib/presentation/registerpage.dart
@@ -8,3 +8,12 @@ class RegisterPage extends StatefulWidget {
   @override
   State<RegisterPage> createState() => _RegisterPageState();
 }
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+
+ 
