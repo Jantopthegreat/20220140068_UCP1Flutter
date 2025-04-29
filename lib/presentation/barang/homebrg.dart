10c51bf72eb9e0824779d84f2f8a27748f2e7c01
Jantopthegreat <fauzangriya@gmail.com>
2025-04-29 22:02:36 +0700

    membuat variabel, text editing controller, dll

--- lib/presentation/barang/homebrg.dart
+++ lib/presentation/barang/homebrg.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
 
 class HomeBrg extends StatefulWidget {
-  const HomeBrg({super.key});
+  final String username;
+  final String avatarPath;
+
+  const HomeBrg({
+    super.key,
+    required this.username,
+    required this.avatarPath,
+  });
 
   @override
   State<HomeBrg> createState() => _HomeBrgState();
 }
 
 class _HomeBrgState extends State<HomeBrg> {
+  final _formKey = GlobalKey<FormState>();
+  final _jumlahController = TextEditingController();
+
+  DateTime? _selectedDate;
+  String? _selectedJenisTransaksi;
+  String? _selectedJenisBarang;
+  int _hargaSatuan = 0;
+
+  final _jenisTransaksiList = ['Barang Masuk', 'Barang Keluar'];
+  final Map<String, int> _barangHargaMap = {
+    'Tenda': 10000,
+    'Carrier': 540000,
+    'Kompor': 80000,
+    'Matras': 30000,
+  };
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
